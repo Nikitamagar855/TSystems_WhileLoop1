567234be6b94f0933848a0e74a80526ee0d3eb7b
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:34:49 +0530

    Add project files.

---
+++ TSystems_WhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================WHILELOOP=================");
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine($"While loop is executed  {i}");
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
